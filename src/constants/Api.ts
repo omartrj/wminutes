cafef7c856144394f9eac7ccaef6b453fbaa4dbf
omartrj <omarcriacci@gmail.com>
2025-11-28 16:38:20 +0100

    Update REFRESH_INTERVAL_MS comment

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -2,4 +2,4 @@
 export const API_BASE_URL = "https://www.wienerlinien.at/ogd_realtime/monitor";
 export const API_PARAMS = "activateTrafficInfo=stoerungkurz&activateTrafficInfo=stoerunglang&activateTrafficInfo=elevatorinfo";
 export const PROXY_URL = "https://corsproxy.io/?";
-export const REFRESH_INTERVAL_MS = 30000; // 30 seconds
+export const REFRESH_INTERVAL_MS = 30000; // Don't go below 15 seconds!! (https://www.data.gv.at/datasets/cfba4373-a654-3e0b-80f8-348738169f95?locale=de)
